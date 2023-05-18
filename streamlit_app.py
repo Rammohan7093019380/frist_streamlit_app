b3e0fb4aed70347fc70a20ca2763e5f5e2072fe3
Rammohan7093019380 <121368441+Rammohan7093019380@users.noreply.github.com>
2023-05-18 10:18:59 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,10 +25,17 @@ streamlit.dataframe(fruityvice_normalized)
 
 import snowflake.connector
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+# Streamlit < 0.65
+from streamlit.ReportThread import get_report_ctx
 
+# Streamlit > 0.65
+from streamlit.report_thread import get_report_ctx
+
+# Streamlit > ~1.3
+from streamlit.script_run_context import get_script_run_ctx as get_report_ctx
+
+# Streamlit > ~1.8
+from streamlit.scriptrunner.script_run_context import get_script_run_ctx as get_report_ctx
+
+# Streamlit > ~1.12
+from streamlit.runtime.scriptrunner.script_run_context import get_script_run_ctx as get_report_ctx
