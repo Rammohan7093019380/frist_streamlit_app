277f48866fab3ce5769f3edbd688f87356d2458a
Rammohan7093019380 <121368441+Rammohan7093019380@users.noreply.github.com>
2023-05-19 12:08:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,24 +70,17 @@ streamlit.stop()
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-
-
-my_cur=my_cnx.cursor() 
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
-my_data_row=my_cur.fetchone()
-my_data_row=my_cur.fetchone()
-streamlit.text("The fruit load list contains:")
-my_data_rows = my_cur.fetchall()
-streamlit.text("The fruit load list contains:")
+streamlit.header("the fruit load list contains:")
 def get_fruit_load_list():
-    with my_cnx.cursor() as my_cur:
-         my_cur.execute("select * from fruit_load_list")
-         return my_cur.fetchall()
-#Add a button to load the fruit
-if streamlit.button('get fruit load list'):
-   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-   my_data_rows=get_fruit_load_list()
-   streamlit.dataframe(my_data_rows) 
+   with my_cnx.cursor() as my_cur:
+        my_cur.excute("select * from fruit_load_list")
+        return my_cur.fetchall()
+   if streamlit.button('get fruit load list'):
+      my_cnx=snowflake.connector.connect(**streamlit.secrets["snowflake"])
+      my_data_rows=get_fruit_load_list()
+      streamlit.dataframe(my_data_rows)
+
+
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','apple')
 def insert_row_snowflake (new_fruit):
