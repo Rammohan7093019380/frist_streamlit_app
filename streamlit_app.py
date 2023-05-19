52293af5e1457e5b84310166c2c4b260eb7cc8ac
Rammohan7093019380 <121368441+Rammohan7093019380@users.noreply.github.com>
2023-05-19 11:06:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -71,18 +71,23 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+
 my_cur=my_cnx.cursor() 
 my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_row=my_cur.fetchone()
 my_data_row=my_cur.fetchone()
 streamlit.text("The fruit load list contains:")
-my_cur.execute("select * from fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.text("The fruit load list contains:")
-streamlit.dataframe(my_data_rows) 
-
-
+def get_fruit_load_list():
+    with my_cnx.cursor() as my_cur:
+         my_cur.execute("select * from fruit_load_list")
+         return my_cur.fetchall()
+#Add a button to load the fruit
+if streamlit.button('get fruit load list')
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   my_data_rows=get_fruit_load_list()
+   streamlit.dataframe(my_data_rows) 
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','apple')
 def insert_row_snowflake (new_fruit):
